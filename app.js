3663e5e9087fece34a8944d1a9cd702bd3cd1abf
pizyumi <piz_yumina@hotmail.com>
2020-01-08 17:04:51 +0900

    [0.2][modification] disable config, database and session temporarily

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var server = require('./server');
 
 module.exports = async () => {
     var c = await config.get();
-    var d = await db.connect(c);
-    var w = await web.get(c, [
+    //var d = await db.connect(c);
+    // var w = await web.get(c, [
+    //     await roguelike.get('')
+    // ]);
+    // var s = await server.start(c, w);
+    var w = await web.get({}, [
         await roguelike.get('')
     ]);
-    var s = await server.start(c, w);
+    var s = await server.start({}, w);
     var end_server_once = _.once(server.end);
     var disconnect_db_once = _.once(db.disconnect);
 
